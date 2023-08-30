67cce487079d5fccea3668789cd47a5b59d14c8d
Jessica Prieto <jessicapr1202@gmail.com>
2023-08-30 20:07:09 -0300

    Despliegue de productos por categoría + filtros de búsqueda

--- js/login.js
+++ js/login.js
@@ -1,20 +1,19 @@
 function authenticate() {
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
-  
-    if (username && password) {
-  
-      localStorage.setItem('authenticated', 'true'); 
-      window.location.href = 'index.html';
-    } else {
-      alert('Por favor ingrese un usuario y contraseña válidos.');
-    }
+  const username = document.getElementById('username').value;
+  const password = document.getElementById('password').value;
+
+  if (username && password) {
+
+    localStorage.setItem('authenticated', 'true');
+    localStorage.setItem('username', username); //se guarda en el localstorage el nombre de usuario 
+    window.location.href = 'index.html';
+  } else {
+    alert('Por favor ingrese un usuario y contraseña válidos.');
   }
-  
-  
-  
-  function isAuthenticated() {
-    return localStorage.getItem('authenticated') === 'true';
-  }
-  
-  
+}
+
+
+
+function isAuthenticated() {
+  return localStorage.getItem('authenticated') === 'true';
+}
